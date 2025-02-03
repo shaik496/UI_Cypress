5bccd63e4cdaf1bed26cc81fef61201e022d1e64
Shaik Meeravali <44121209+shaik496@users.noreply.github.com>
2025-02-03 22:27:55 +0530

    Adding files

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,11 @@
 import testData from '/Users/meeravalishaik/Desktop/Shaik/WorkSpace/Active/UI_Cypress/cypress/fixtures/testData.json';
 Cypress.Commands.add("LaunchURL", () => {
-    cy.visit(testData.testu);
-    cy.visit(testData.testu);
-    cy.title().should('eq', 'Register: Mercury Tours122');
+    cy.visit(testData.testUR);
+    cy.title().should('eq', 'Register: Mercury Tours');
+  });
+
+
+  Cypress.Commands.add("DropDownValue", (xpath,value) => {
+    cy.get(xpath).select(value);
+  
   });
--- cypress/support/step_definitions/registrationSteps.js
+++ cypress/support/step_definitions/registrationSteps.js
@@ -1,7 +1,10 @@
-import './commands'; 
+import '../commands'; // Correct the import path to reference the commands file
 const { Given } = require('@badeball/cypress-cucumber-preprocessor');
 
-
 Given('I am on the registration page', () => {
-  cy.LaunchURL();  
+  cy.LaunchURL(); 
+  cy.get("[name='firstName']").type("John");
+  cy.get("[name='lastName']").type("AB");
+  cy.DropDownValue("[name='country']", "INDIA");
+
 });
